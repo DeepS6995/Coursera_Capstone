f30e208cb4e02b646ea10c15e4255265759d1da9
DeepS6995 <DeepS6995@users.noreply.github.com>
2018-10-08 21:45:50 -0400

    Add files via upload
    This is the newer file. It has description in markdown.

---
+++ Coursera_Capstone-Copy1 (1).py
@@ -0,0 +1,23 @@
+
+# coding: utf-8
+
+# ## This is a program in Coursera Capstone Project 
+# ### I have written three lines as required
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course')
+
